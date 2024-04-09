b5fcb8eb0feb0253a6e70d1a9db588b41f051d2d
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2024-04-09 21:29:55 +0200

    add EvenNumberFilterTest&FileServiceTest&MyServiceTest&ImportedCSVTextProcessorTest

--- amber/src/main/java/com/example/transform/ImportedCSVTextProcessor.java
+++ amber/src/main/java/com/example/transform/ImportedCSVTextProcessor.java
@@ -3,13 +3,12 @@ package com.example.transform;
 public class ImportedCSVTextProcessor {
 
     String process(String line) {
-        // TODO: implement here
-        // 1. replace "," with space
-        // 2. remove "none"
-        // 3. remove white spaces from start and end
-        // 4. add "-done" at the end
-        // 5. to uppercase
+        line = line.replace(",", " ");
+        line = line.replace("none", "");
+        line = line.trim();
+        line = line + "-done";
+        line = line.toUpperCase();
 
-        return "";
+        return line;
     }
 }
